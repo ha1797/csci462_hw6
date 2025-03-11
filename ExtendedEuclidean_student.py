369e8dc3258078d9813a31feff9bcb14ccfbc375
Hoja Arzanesh <ha1797@rit.edu>
2025-03-11 00:26:45 -0400

    Fix bad formatting in student copy.
    Fixed formatting so each function is separated by two newlines, instead of just one.

--- ExtendedEuclidean_student.py
+++ ExtendedEuclidean_student.py
@@ -8,6 +8,7 @@ def gcd(r0, r1):
         r1 = r2
     return r0
 
+
 def eea(r0, r1):
     # (r0, s0, t0), (r1, s1, t1), (r2, s2, t2), (r3, s3, t3), ..., (rn, sn, tn), 0 
     # ri = si*r0 + ti*r1
@@ -30,15 +31,18 @@ def eea(r0, r1):
         t0 = t1
         t1 = t2
 
+
 def inverse(r0, r1):
     # Complete this function
     return 0
+
 
 def main():
     print(inverse(973, 301))
     print(inverse(973, 300))
     print(inverse(97333333333333, 300000000))
     print(inverse(97333333333333, 5678901234))
+
 
 if __name__ == "__main__":
     main()
